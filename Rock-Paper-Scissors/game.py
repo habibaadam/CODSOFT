3a8e66e208606587a0ded2ff985b55d1bb1dfc68
habibaadam <habiepalmer@gmail.com>
2023-12-26 17:54:35 +0000

    fixed bugs

--- Rock-Paper-Scissors/game.py
+++ Rock-Paper-Scissors/game.py
@@ -3,13 +3,18 @@
 """Command-line rock paper scissors game"""
 import random
 
+#Tracking number of wins
+user_wins = 0
+computer_wins = 0
+
+
 while True:
     print("Welcome To My Rock Paper Scissors Game 🌟🖤")
     print("1 For Rock 🪨\n2 For Paper📄\n3 For Scissors ✂️ \nLet's Hope You're Lucky!🤣")
 
     rock_art = """
 
-     ---'   ____)
+     ---'    ____)
             (_____)
             (_____)
             (____)
@@ -38,83 +43,83 @@ while True:
 
         """
 
-    tie_art = """
-      _   _
-     | | (_)
-     | |_ _  ___
-     | __| |/ _ \
-     | |_| |  __/
-      \__|_|\___|
+    for _ in range(3):
+        u_choice = int(input("Enter your choice: "))
+        try:
+            u_choice_err = int(u_choice)
+        except ValueError:
+            print("Please enter a number between 1 and 3")
+            continue
 
-        """
+        computers_choice = random.randint(1, 3)
+        """Tie Instances"""
+        if u_choice == 1 and computers_choice == 1:
+            print("You choose rock")
+            print(rock_art)
+            print("Computer chose rock")
+            print(rock_art)
+            print("\nIT'S A TIE!!!")
+        elif u_choice == 2 and computers_choice == 2:
+            print("You choose paper")
+            print("Computer chose paper")
+            print("\nIT'S A TIE!!!")
+        elif u_choice == 3 and computers_choice == 3:
+            print("You chose scissors")
+            print("Computer chose scissors")
+            print("\nIT'S A TIE!!!")
 
-    u_choice = int(input("Enter your choice: "))
-    try:
-        u_choice_err = int(u_choice)
-    except ValueError:
-        print("Please enter a number between 1 and 3")
-        continue
+        """Win Instances For User"""
+        if u_choice == 1 and computers_choice == 3:
+            print("You chose rock")
+            print(rock_art)
+            print("Computer chose scissors")
+            print(scissors_art)
+            print("You Win!!!")
+            user_wins += 1
+        elif u_choice == 2 and computers_choice == 1:
+            print("You chose paper")
+            print(paper_art)
+            print("Computer chose rock")
+            print(rock_art)
+            print("You Win!!!")
+            user_wins += 1
+        elif u_choice == 3 and computers_choice == 2:
+            print("You chose scissors")
+            print(scissors_art)
+            print("Computer chose paper")
+            print(paper_art)
+            print("You Win!!!")
+            user_wins += 1
 
-    computers_choice = random.randint(1, 3)
-    """Tie Instances"""
-    if u_choice == 1 and computers_choice == 1:
-        print("You choose rock")
-        print(rock_art)
-        print("Computer chose rock")
-        print(rock_art)
-        print("Its a tie!!!")
-        print(tie_art)
-    elif u_choice == 2 and computers_choice == 2:
-        print("You choose paper")
-        print("Computer chose paper")
-        print("Its a tie!!!")
-        print(tie_art)
-    elif u_choice == 3 and computers_choice == 3:
-        print("You chose scissors")
-        print("Computer chose scissors")
-        print("Its a tie!!!")
-        print(tie_art)
 
-    """Win Instances For User"""
-    if u_choice == 1 and computers_choice == 3:
-        print("You chose rock")
-        print(rock_art)
-        print("Computer chose scissors")
-        print(scissors_art)
-        print("You Win!!!")
-    elif u_choice == 2 and computers_choice == 1:
-        print("You chose paper")
-        print(paper_art)
-        print("Computer chose rock")
-        print(rock_art)
-        print("You Win!!!")
-    elif u_choice == 3 and computers_choice == 2:
-        print("You chose scissors")
-        print(scissors_art)
-        print("Computer chose paper")
-        print(paper_art)
-        print("You Win!!!")
 
-    """Win Instances For Computer"""
-    if u_choice == 1 and computers_choice == 2:
-        print("You chose rock")
-        print(rock_art)
-        print("Computer chose paper")
-        print(paper_art)
-        print("You Lose!!!")
-    elif u_choice == 2 and computers_choice == 3:
-        print("You chose paper")
-        print(paper_art)
-        print("Computer chose scissors")
-        print(scissors_art)
-        print("You Lose!!!")
-    elif u_choice == 3 and computers_choice == 1:
-        print("You chose scissors")
-        print(scissors_art)
-        print("Computer chose rock")
-        print(rock_art)
-        print("You Lose!!!")
+        """Win Instances For Computer"""
+        if u_choice == 1 and computers_choice == 2:
+            print("You chose rock")
+            print(rock_art)
+            print("Computer chose paper")
+            print(paper_art)
+            print("You Lose!!!")
+            computer_wins += 1
+        elif u_choice == 2 and computers_choice == 3:
+            print("You chose paper")
+            print(paper_art)
+            print("Computer chose scissors")
+            print(scissors_art)
+            print("You Lose!!!")
+            computer_wins += 1
+        elif u_choice == 3 and computers_choice == 1:
+            print("You chose scissors")
+            print(scissors_art)
+            print("Computer chose rock")
+            print(rock_art)
+            print("You Lose!!!")
+            computer_wins += 1
+
+    print(f"Your total score is : {user_wins}")
+    print(f"Computer's total score is : {computer_wins}")
 
     another_round = input("Do you want to play another round? (yes/no): ")
     if another_round.lower() != "yes":
+        print("Thanks for playing!")
         break
